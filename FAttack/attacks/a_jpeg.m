f867aa62a271746a7f8466225b714947ec80525f
francescotescari <36578133+francescotescari@users.noreply.github.com>
2019-11-05 10:15:25 +0100

    un po meno schifo

--- FAttack/attacks/a_jpeg.m
+++ FAttack/attacks/a_jpeg.m
@@ -1,7 +1,7 @@
 function [img, inputs] = a_jpeg(image,factor)
 quality = (1-factor)*100;
 inputs = [quality];
-fn = ['jp', num2str(floor(now*100000000-floor(now*100)*1000000)), num2str(sum(image(:))), num2str(round(quality.*100)), num2str(randi(10000000)), '.jpg'];
+fn = ['jp', num2str(floor(now*100000000000-floor(now*100)*1000000000)), num2str(sum(image(:))), num2str(round(quality.*100)), num2str(randi(10000000)), '.jpg'];
 img = test_jpeg(uint8(image), quality, fn);
 
 end
