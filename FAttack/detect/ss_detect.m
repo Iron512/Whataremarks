ce375eaa9d3a34d09caca4683840e4a62d850e05
francescotescari <36578133+francescotescari@users.noreply.github.com>
2019-11-05 09:17:06 +0100

    att_fix

--- FAttack/detect/ss_detect.m
+++ FAttack/detect/ss_detect.m
@@ -1,27 +1,30 @@
-function detect_function = ss_detect(threshold, alpha, wsize)
-    function [result, wpsnr, sim] = detect_fn(originalImage, watermarkedImage, attackedImage)
+
+function [result, wpsnr, sim] = ss_detect(originalImage, watermarkedImage, attackedImage)
+alpha = 15;
         watermarkedImage = imread(watermarkedImage);
         originalImage = imread(originalImage);
         attackedImage = imread(attackedImage);
-        [dimx,dimy] = size(originalImage);
-        It = dct2(double(originalImage));
-        It_re = reshape(It,1,dimx*dimy);
+        
+        
+        [~,U] = lu(dct2(double(originalImage)));
+        [dimx,dimy] = size(U);
+        It_re = reshape(U,1,dimx*dimy);
         [~,Ix] = sort(abs(It_re),'descend');
         
-        Itw = dct2(double(watermarkedImage));
-        It_rew = reshape(Itw,1,dimx*dimy);
+        [~,Uw] = lu(dct2(double(watermarkedImage)));
+        It_rew = reshape(Uw,1,dimx*dimy);
         
-        Ita = dct2(double(attackedImage));
-        It_rea = reshape(Ita,1,dimx*dimy);
+        [~,Ua] = lu(dct2(double(attackedImage)));
+        It_rea = reshape(Ua,1,dimx*dimy);
         
         ew = zeros(1, wsize);
         ewa = zeros(1, wsize);
         
-        k = 2;
+        k = 100;
         for j = 1:wsize
             m = Ix(k);
-            ew(j) = max(min(round((It_rew(m)/It_re(m)-1)/alpha), 1),0);
-            ewa(j) = max(min(round((It_rea(m)/It_re(m)-1)/alpha), 1),0);           
+            ew(j) = max(min(round((It_rew(m)-It_re(m))/alpha), 1),0);
+            ewa(j) = max(min(round((It_rea(m)-It_re(m))/alpha), 1),0);           
             k = k+1;
         end
         sim = (ew*(ewa'))/sqrt(ewa*ewa');
@@ -29,7 +32,7 @@ function detect_function = ss_detect(threshold, alpha, wsize)
             sim = 0;
         end
         
-        if sim > threshold
+        if sim > 14
             result = 1;
         else
             result = 0;
@@ -37,5 +40,4 @@ function detect_function = ss_detect(threshold, alpha, wsize)
         
         wpsnr = WPSNR(attackedImage, watermarkedImage); 
     end
-    detect_function = @detect_fn;
-end
+    
